54f7f2f739f75998c2cbe429175bd89b6b110732
Pugsley <0xpugsley@protonmail.com>
2022-06-26 14:40:47 +0200

    fp7

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib ( someFunc )
+import Fp7 (perfects, pyths, scalarProduct)
+import Lib (someFunc)
 
 main :: IO ()
-main = someFunc
+main = do
+  print "------"
+  print (pyths 5)
+  print (perfects 500)
+  print (scalarProduct [1, 2, 3] [1, 2, 3])
---
+++ src/Fp7.hs
@@ -0,0 +1,20 @@
+module Fp7
+  ( pyths,
+    perfects,
+    scalarProduct,
+  )
+where
+
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1 .. n], y <- [1 .. n], z <- [1 .. n], x ^ 2 + y ^ 2 == z ^ 2]
+
+factors :: Int -> [Int]
+factors x = [i | i <- [1 .. x], x `mod` i == 0]
+
+-- factors x = [i | i <- [1 .. x], (x `mod` i == 0) && (i /= x)]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1 .. n], sum (init (factors x)) == x]
+
+scalarProduct :: [Int] -> [Int] -> Int
+scalarProduct xs ys = sum [y * y | (x, y) <- zip xs ys]
