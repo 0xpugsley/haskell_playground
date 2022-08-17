310a0b360c75c150e4e48033e3fbe80aac6727c3
Pugsley <0xpugsley@protonmail.com>
2022-08-17 21:03:15 +0200

    bleah

--- 4game/Main.hs
+++ 4game/Main.hs
@@ -40,7 +40,7 @@ data Player = O | B | X deriving (Eq, Ord)
 
 instance Show Player where
   show O = "O"
-  show B = "."
+  show B = "B"
   show X = "X"
 
 -- instance Show Board where
@@ -63,12 +63,6 @@ createEmptyBoard = replicate rows createRow
 createTestBoard :: Board
 createTestBoard = [[B, B, X], [B, X, O], [O, O, X]]
 
--- 1. Produce the game tree to specified depth
--- 2. Label each leaf with winner, or B if not over or draw
--- 3. Then work up the tree - if O to play, take min of the children (O<B<X)
--- if X to play, take max of children
--- 4 Best move is one with same label as root.
-
 data Tree a = Node a [Tree a] deriving (Show)
 
 columns :: Board -> Board
@@ -76,16 +70,6 @@ columns = transpose
 
 choice :: Player -> Row -> Row
 choice p r = drop 1 (takeWhile (== B) r) ++ [p] ++ dropWhile (== B) r
-
-createChildrenRaw :: Player -> Board -> [Board]
-createChildrenRaw p b = map (\(r, i) -> replace i r b) (filter (\(e, i) -> B `elem` e) (zip b [0 .. length b]))
-  where
-    replace i r b' = first ++ choice p r : drop 1 second
-      where
-        (first, second) = splitAt i b'
-
-createChildren :: Player -> Board -> [Board]
-createChildren p b = map columns (createChildrenRaw p (columns b))
 
 next :: Player -> Player
 next X = O
@@ -106,15 +90,84 @@ winner r =
 l :: Board
 l = [[O, B, O], [X, X, X], [B, X, B]]
 
+ll = [[X, O, X, X], [X, X, O, O], [X, O, X, O]]
+
+lll = [[X, O, X, O], [X, O, X, O], [O, X, O, X]]
+
+a = [[B, B, X], [B, X, O], [O, O, X]]
+
+badX = [[X, O, X], [X, X, O], [O, O, X]]
+
+badB = [[O, X, X], [X, X, O], [O, O, X]]
+
+shift :: Int -> Int -> [Player] -> [Player]
+shift n len l = replicate (len - n) B ++ l ++ replicate n B
+
+-- shift n len l = take (length l) (drop n (cycle l))
+
+shiftedFromTop :: Board -> Board
+shiftedFromTop b = [shift i len r | (r, i) <- zip b [0 .. length b]]
+  where
+    len = length b - 1
+
+shiftedFromBottom :: Board -> Board
+shiftedFromBottom b = [shift i len r | (r, i) <- zip b (reverse [0 .. length b])]
+  where
+    len = length b - 1
+
+-- idk maybe can be cut earlier
 label :: Board -> Player
 label = foldr (\r p -> if p == B then winner r else p) B
 
-createGameTreeRaw :: Player -> Int -> Board -> Tree Board
-createGameTreeRaw _ 0 board = Node board []
-createGameTreeRaw player depth' board = Node board (map (createGameTreeRaw (next player) (depth' - 1)) (createChildren player board))
+whoIsWinner :: Board -> Player
+whoIsWinner b = case filter (/= B) [rows, cols, diagonal, contrDiagonal] of
+  [x] -> x
+  (x : xs) -> if all (== x) xs then x else B
+  [] -> B
+  where
+    rows = label b
+    cols = label (columns b)
+    diagonal = label (columns (shiftedFromTop b))
+    contrDiagonal = label (columns (shiftedFromBottom b))
 
-createGameTree :: Player -> Board -> Tree Board
+createChildrenRaw :: Player -> Board -> [Board]
+createChildrenRaw p b = map (\(r, i) -> replace i r b) (filter (\(e, i) -> B `elem` e) (zip b [0 .. length b]))
+  where
+    replace i r b' = first ++ choice p r : drop 1 second
+      where
+        (first, second) = splitAt i b'
+
+createChildren :: Player -> Board -> [Board]
+createChildren p b = map columns (createChildrenRaw p (columns b))
+
+createGameTreeRaw :: Player -> Int -> Board -> Tree (Board, Player)
+createGameTreeRaw p 1 board = Node (board, p) []
+createGameTreeRaw player depth' board = Node (board, player) (map (createGameTreeRaw (next player) (depth' - 1)) (createChildren player board))
+
+createGameTree :: Player -> Board -> Tree (Board, Player)
 createGameTree player = createGameTreeRaw player depth
+
+labelTree :: Tree (Board, Player) -> Tree (Board, Player, Player)
+labelTree (Node (b, turn) []) = Node (b, turn, whoIsWinner b) []
+labelTree (Node (b, turn) l) = Node (b, turn, B) (map labelTree l)
+
+-- 1. Produce the game tree to specified depth
+-- 2. Label each leaf with winner, or B if not over or draw
+-- 3. Then work up the tree - if O to play, take min of the children (O<B<X)
+-- if X to play, take max of children
+-- 4 Best move is one with same label as root.
+
+untree :: [Tree (Board, Player, Player)] -> [Player]
+untree = map (\(Node (_, _, l) _) -> l)
+
+testNode :: Tree ([[Player]], Player)
+testNode = Node ([[B, B, X], [B, X, O], [O, O, X]], B) [Node ([[B, B, X], [B, X, O], [O, O, X]], X) [Node ([[B, B, X], [B, X, O], [O, O, X]], X) []]]
+
+workUpTheTree :: Tree (Board, Player, Player) -> Tree (Board, Player, Player)
+workUpTheTree (Node (b, t, l) []) = Node (b, t, l) []
+workUpTheTree (Node (b, t, l) xs) = Node (b, t, compute (map workUpTheTree xs)) xs
+  where
+    compute = (if t == O then minimum else maximum) . untree
 
 main :: IO ()
 main = do
